260e4ed2006ad7e5f1399dcfd6153cedcf382976
christophe <clalatahiana@gmail.com>
2020-10-20 16:59:53 +0200

    add projectiles

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Diagnostics;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -9,15 +8,15 @@ public class Player : MonoBehaviour
     [SerializeField]
     private Camera m_camera;
     [SerializeField]
-    private float m_verticalSpeed;
+    private float m_verticalSpeed, m_horizontalSpeed;
     [SerializeField]
-    private float m_horizontalSpeed;
+    // time between projectiles
+    private float projectileCD;
+    [SerializeField]
+    GameObject projectile;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Stopwatch stopWatch;
+
 
     // Update is called once per frame
     void Update()
@@ -30,26 +29,44 @@ public class Player : MonoBehaviour
         // set screen position limit
         Vector3 screenPos = m_camera.WorldToScreenPoint(transform.position);
         
+        // move forward
         if (Input.GetKey(KeyCode.UpArrow) && screenPos.y <= Screen.height)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y + m_horizontalSpeed * Time.deltaTime, 0);
+            transform.position = new Vector3(transform.position.x, transform.position.y + m_verticalSpeed * Time.deltaTime, 0);
         }
-        
+
+        // move backward
         if (Input.GetKey(KeyCode.DownArrow) && screenPos.y >= 0)
         {
-            transform.position = new Vector3(transform.position.x, transform.position.y - m_horizontalSpeed * Time.deltaTime, 0);
+            transform.position = new Vector3(transform.position.x, transform.position.y - m_verticalSpeed * Time.deltaTime, 0);
         }
 
+        // move right
         if (Input.GetKey(KeyCode.RightArrow ) && screenPos.x <= Screen.width)
         {
-            transform.position = new Vector3(transform.position.x + m_verticalSpeed * Time.deltaTime, transform.position.y, 0);
+            transform.position = new Vector3(transform.position.x + m_horizontalSpeed * Time.deltaTime, transform.position.y, 0);
         }
 
+        // move left
         if (Input.GetKey(KeyCode.LeftArrow) && screenPos.x >= 0)
         {
-            transform.position = new Vector3(transform.position.x - m_verticalSpeed * Time.deltaTime, transform.position.y, 0);
+            transform.position = new Vector3(transform.position.x - m_horizontalSpeed * Time.deltaTime, transform.position.y, 0);
         }
 
+        // fire bullet
+        if (Input.GetKey(KeyCode.Space) && stopWatch.ElapsedMilliseconds > projectileCD)
+        {
+            Instantiate(projectile, new Vector3(transform.position.x, transform.position.y, 0), Quaternion.identity);
+            stopWatch.Restart();
+        }
+
+
+    }
+
+    private void Awake()
+    {
+        stopWatch = new Stopwatch();
+        stopWatch.Start();
 
     }
 }
