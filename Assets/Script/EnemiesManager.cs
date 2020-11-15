c552d3c31287ed6e61488044d3bacd85fea87da2
christophe <clalatahiana@gmail.com>
2020-11-15 05:16:13 +0100

    enemy formation

--- Assets/Script/EnemiesManager.cs
+++ Assets/Script/EnemiesManager.cs
@@ -3,18 +3,26 @@ using UnityEngine;
 
 public class EnemiesManager : MonoBehaviour
 
-{    //This field gets serialized even though it is private
-     //because it has the SerializeField attribute applied.
+{
+    [System.Serializable]
+    public class GroupEnemies
+    {
+        public string groupName;
+        public GameObject enemy;
+        public int count;
+        public enum EnemyFormation { ARCUP, ARCDOWN, CENTER, RANDOM }
+        public EnemyFormation enemyFormation = EnemyFormation.RANDOM;
+    }
+
     [System.Serializable]
     public class Wave
     {
-        public enum EnemyFormation { V, FORWARDARC, RANDOM}
-        public EnemyFormation enemyFormation = EnemyFormation.RANDOM;
-        public GameObject enemy;
-        public int count; // number of enemies
-        public float rate;
-        public bool isBoss;
+        public string wavename;
+        public GroupEnemies[] enemiesGroup;
+        public float timeBetweenGroups;
     }
+
+
 
     [SerializeField]
     private Wave[] waves;
@@ -32,16 +40,20 @@ public class EnemiesManager : MonoBehaviour
 
     private void Update()
     {
+        // if all waves not cleared
         if (!cleared)
         {
+            // if wave completed
             if (state == SpawnState.WAITING)
             {
                 WaveCompleted();
                 return;
             }
 
+            // if times between waves reached
             if (waveCountdown <= 0)
             {
+                // if not spawning already
                 if (state != SpawnState.SPAWNING)
                 {
                     // start spawning
@@ -51,6 +63,7 @@ public class EnemiesManager : MonoBehaviour
             }
             else
             {
+                // countdown
                 waveCountdown -= Time.deltaTime;
             }
         }
@@ -58,8 +71,9 @@ public class EnemiesManager : MonoBehaviour
     }
     private void WaveCompleted()
     {
-
+        // update state to counting
         state = SpawnState.COUNTING;
+        // reset wave countdown
         waveCountdown = timeBetweenWaves;
 
         // if no more incoming wave
@@ -75,27 +89,65 @@ public class EnemiesManager : MonoBehaviour
 
     IEnumerator SpawnWave(Wave wave)
     {
+        // update state to spwaning
         state = SpawnState.SPAWNING;
-        for (int i = 0; i < wave.count; i++)
+        for (int i = 0; i < wave.enemiesGroup.Length; i++)
         {
-            SpawnEnemy(wave.enemy.transform);
-            yield return new WaitForSeconds(1f/ wave.rate);
+            // spawn group of enemies
+            SpawnEnemies(wave.enemiesGroup[i]);
+
+            // wait between 2 groups of enemies
+            yield return new WaitForSeconds(wave.timeBetweenGroups);
 
         }
 
+        // finished spawn, update state to waiting
         state = SpawnState.WAITING;
         yield break;
         
     }
 
 
-    void SpawnEnemy(Transform enemy)
+    void SpawnEnemies(GroupEnemies enemies)
     {
-        
-        Vector3 point;
+        int max = enemies.count;
         float z = m_camera.transform.position.z;
-        point = m_camera.ScreenToWorldPoint(new Vector3(Random.Range(0, Screen.width), Screen.height, z));
-        Instantiate(enemy, point, enemy.rotation);
+        float x0 = Screen.width / 2;
+        float r = Screen.width / 2 - 50;
+        float y0 = Screen.height;
+        Vector3 point;
+        float angle = Mathf.PI / (max - 1); 
+        for (int i = 0; i < max; i++)
+        {
+            // spwan in arc up shape
+            if (enemies.enemyFormation == GroupEnemies.EnemyFormation.ARCDOWN)
+            {
+                float x = x0 + ( r * Mathf.Cos( Mathf.PI + (angle * i) ) );
+                float y = y0 + r + ( r * Mathf.Sin( Mathf.PI + (angle * i) ) );
+                point = m_camera.ScreenToWorldPoint(new Vector3(x, y, z));
+            }
+            // spawn in arc down shape
+            else if (enemies.enemyFormation == GroupEnemies.EnemyFormation.ARCUP)
+            {
+                float x = x0 + (r * Mathf.Cos(angle * i));
+                float y = y0 + (r * Mathf.Sin(angle * i));
+                point = m_camera.ScreenToWorldPoint(new Vector3(x, y, z));
+            }
+            // spawn in arc down shape
+            else if (enemies.enemyFormation == GroupEnemies.EnemyFormation.CENTER)
+            {
+                point = m_camera.ScreenToWorldPoint(new Vector3(x0, y0, z));
+            }
+            // random spawning
+            else
+            {
+                point = m_camera.ScreenToWorldPoint(new Vector3( Random.Range(2, Screen.width - 2), Random.Range(Screen.height, y0 + r ), z) ); 
+
+            }
+            // instantiate ennemies
+            Instantiate(enemies.enemy, point, enemies.enemy.transform.rotation);
+        }    
+        
     }
 
     private void Awake()
